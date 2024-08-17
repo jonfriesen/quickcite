2ab0cfe9be2231e288dfd32bdc77536f9458ab62
Jon Friesen <jon@jonfriesen.ca>
2024-08-17 09:59:22 -0600

    extracts clipboard icon to file / adds to theme picker

--- content.js
+++ content.js
@@ -8,11 +8,14 @@ function injectButton() {
 	const button = document.createElement('button')
 	button.id = 'pr-copy-button'
 	button.className = `pr-copy-button ${selectedStyle}`
-	button.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.5em; height: 1.5em;">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
-    </svg>
-  `
+	button.innerHTML = `<img src="${chrome.runtime.getURL('dist/clipboard.svg')}" alt="Copy PR Info">`
+
+	// Inject the shared styles
+	const style = document.createElement('link')
+	style.rel = 'stylesheet'
+	style.type = 'text/css'
+	style.href = chrome.runtime.getURL('dist/buttons.css')
+	;(document.head || document.documentElement).appendChild(style)
 
 	button.addEventListener('mouseenter', () => {
 		button.style.transform = 'scale(1.05)'
@@ -58,13 +61,6 @@ function injectButton() {
 				console.error('Failed to copy text: ', err)
 			})
 	})
-
-	// Inject the shared styles
-	const style = document.createElement('link')
-	style.rel = 'stylesheet'
-	style.type = 'text/css'
-	style.href = chrome.runtime.getURL('./dist/buttons.css')
-	;(document.head || document.documentElement).appendChild(style)
 
 	document.body.appendChild(button)
 }
